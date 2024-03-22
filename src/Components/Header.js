24807e8b2d6dd28ae72cec076cf450dfc911a216
Aarish Ansari <aarishans775@gmail.com>
2024-03-22 14:26:14 +0530

    Initial Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import react from 'react';
+import '../Style/Header.css'
+
+function Header() {
+  return (
+    <div className="container">
+       <div style={{display:'-webkit-inline-flex',justifyContent: 'revert',gap:'80px',margin: '0px 15px'}}>
+       <p>Home</p>
+       <p>Contact Us</p>
+       <p>Registration</p>
+       <p>EMail</p>
+       <p>About</p>
+       
+       </div>
+    </div>
+  );
+}
+
+export default Header;
